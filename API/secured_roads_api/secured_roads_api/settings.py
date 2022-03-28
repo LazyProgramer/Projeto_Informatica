d2cc54a7b38887158dd5c551ef3535c0a73b7b06
SousaUA <hsousa@ua.pt>
2022-03-28 10:40:02 +0100

    connected to a postgres database #2

--- API/secured_roads_api/secured_roads_api/settings.py
+++ API/secured_roads_api/secured_roads_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'secured_roads_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'secured_roads',
+        'USER': 'admin',
+        'PASSWORD': 'pi_2122_g2',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
